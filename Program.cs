085c0dc248ef840d1c825b2aa1a9b24b8b993f8f
HenryKasai <henrykasai1775@gmail.com>
2020-04-27 08:31:34 -0300

    Programa que converte milhas para quilômetros.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace MilhasParaKm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Descubra quantos quilômetros é determinada quantidade de milhas.");
+            string milhas = Console.ReadLine();
+            int milha = Convert.ToInt32(milhas);
+            string kms = "1,609";
+            decimal km = Convert.ToDecimal(kms);
+            decimal resultado = milha * km;
+            Console.WriteLine(resultado);
+            Console.ReadKey();
+        }
+    }
+}
